ec4f3ebdd93321a040ed73d8ea519a4d535333ef
Dihee <hwangjihee94@gmail.com>
2022-07-08 19:58:46 +0900

    chore: 초기세팅

---
+++ Airbnb/Airbnb/Home/View/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Airbnb
+//
+//  Created by Jihee hwang on 2022/07/07.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+    }
+}
+

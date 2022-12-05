f536b75d663bb890405d05800052e3b489c8365d
Dihee <hwangjihee94@gmail.com>
2022-12-05 15:27:03 +0900

    chore: 불필요한 주석 제거

--- Airbnb/Airbnb/Presentation/Util/Delegate&DataSource/MapTableViewDataSource.swift
+++ Airbnb/Airbnb/Presentation/Util/Delegate&DataSource/MapTableViewDataSource.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 final class MapTableViewDataSource: NSObject, UITableViewDataSource {
+    private let titles = ["위치", "날짜", "여행자"]
+    private let descriptions = ["양재역", "2023-01-01", "성인 1"]
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { 3 }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: MapTableViewCell.identifier, for: indexPath) as? MapTableViewCell else { return UITableViewCell() }
-        cell.configureTitleInMapTableViewCell(title: "dddd")
-        cell.configureDescriptionInMapTableViewCell(description: "ddddddddddd")
+        
+        cell.configureTitleInMapTableViewCell(title: titles[indexPath.row])
+        cell.configureDescriptionInMapTableViewCell(description: descriptions[indexPath.row])
         return cell
     }
 }
